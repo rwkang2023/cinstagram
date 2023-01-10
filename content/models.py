d1a70af43a721cb16ef6c9168d3951d80fabf06f
rwkang@naver.com <kangrowon@gmail.com>
2023-01-11 01:21:25 +0900

    modify content_feed.user_id

--- content/models.py
+++ content/models.py
@@ -7,7 +7,7 @@ class Feed(models.Model):
     content = models.TextField(help_text="언제는 빼도, [어디]인지는 적어 주세요!")  # 글 내용
     image = models.TextField()  # 피드 이미지
     profile_image = models.TextField()  # 프로필 이미지
-    user_id = models.TextField()  # 글쓴이
+    user_id = models.TextField(max_length=100)  # 글쓴이
     user_email = models.CharField(max_length=100, blank=True, help_text="Email 좀 적어 주세요!")
 
     # class EmailSet(models.TextChoices):
